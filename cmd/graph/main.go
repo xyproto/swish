a97c8128c63e515f11f1eb3c1720dce8481f9b49
Alexander F. Rødseth <rodseth@gmail.com>
2019-06-04 12:24:29 +0200

    Add an example application

---
+++ cmd/graph/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	tm "github.com/buger/goterm"
+	"github.com/xyproto/swish"
+)
+
+func main() {
+	tm.Clear()
+	tm.MoveCursor(0, 0)
+
+	tm.Println("\n" + tm.Bold(tm.Color("The SWISH activation function: ", tm.WHITE)) + tm.Color("1 / (1 + exp(-x))", tm.BLUE) + "\n")
+
+	// Use 70% of the terminal width
+	width := int(float64(tm.Width()) * 0.7)
+
+	chart := tm.NewLineChart(width, 60)
+	chart.Flags = tm.DRAW_RELATIVE
+	data := new(tm.DataTable)
+	data.AddColumn(" x ")
+	data.AddColumn(" y ")
+
+	for x := -5.0; x <= 3.0; x += (8.0 / float64(width)) {
+		data.AddRow(x, swish.F(x))
+	}
+
+	tm.Println(chart.Draw(data))
+	tm.Flush()
+}
