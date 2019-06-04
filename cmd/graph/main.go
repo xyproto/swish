8239ea15f03e04ffc9a57bb50c9c3b8a1b5e0bbb
Alexander F. Rødseth <rodseth@gmail.com>
2019-06-04 12:52:10 +0200

    Update the example to also draw the Sigmoid function

--- cmd/graph/main.go
+++ cmd/graph/main.go
@@ -9,7 +9,7 @@ func main() {
 	tm.Clear()
 	tm.MoveCursor(0, 0)
 
-	tm.Println("\n" + tm.Bold(tm.Color("The SWISH activation function: ", tm.WHITE)) + tm.Color("x / (1 + exp(-x))", tm.BLUE) + "\n")
+	tm.Println("\n" + tm.Bold(tm.Color("The Sigmoid activation function: ", tm.WHITE)) + tm.Color("1 / (1 + exp(-x))", tm.BLUE) + "\n")
 
 	// Use 70% of the terminal width
 	width := int(float64(tm.Width()) * 0.7)
@@ -22,9 +22,25 @@ func main() {
 	data.AddColumn(" y ")
 
 	for x := -5.0; x <= 3.0; x += (8.0 / float64(width)) {
+		data.AddRow(x, swish.Sigmoid(x))
+	}
+
+	tm.Println(chart.Draw(data))
+	tm.Flush()
+
+	tm.Println("\n" + tm.Bold(tm.Color("The SWISH activation function: ", tm.WHITE)) + tm.Color("x / (1 + exp(-x))", tm.BLUE) + "\n")
+
+	chart = tm.NewLineChart(width, height)
+	chart.Flags = tm.DRAW_RELATIVE
+	data = new(tm.DataTable)
+	data.AddColumn(" x ")
+	data.AddColumn(" y ")
+
+	for x := -5.0; x <= 3.0; x += (8.0 / float64(width)) {
 		data.AddRow(x, swish.F(x))
 	}
 
 	tm.Println(chart.Draw(data))
 	tm.Flush()
+
 }
