a5bf579faa5d3963332165f617361703ea2c1bed
Alexander F. Rødseth <rodseth@gmail.com>
2019-06-06 12:07:50 +0200

    Add test function

--- swish_test.go
+++ swish_test.go
@@ -1,6 +1,7 @@
 package swish
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -33,5 +34,25 @@ func BenchmarkSwishPrecise03(b *testing.B) {
 	// run the SwishPrecise function b.N times
 	for n := 0; n < b.N; n++ {
 		SwishPrecise(0.3)
+	}
+}
+
+func abs(a float64) float64 {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func TestSwish(t *testing.T) {
+	diff := abs(Swish(0.7) - SwishPrecise(0.7))
+	fmt.Printf("%3.5f\n", diff)
+	if diff >= 0.0002 {
+		t.Fail()
+	}
+	diff = abs(Swish(0.3) - SwishPrecise(0.3))
+	fmt.Printf("%3.5f\n", diff)
+	if diff >= 0.00002 {
+		t.Fail()
 	}
 }
